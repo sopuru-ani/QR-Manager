6c259e3351791aad9c34c6097b3aeddf9550d3ec
sopuru-ani <sopuruani41@gmail.com>
2025-12-04 10:31:43 -0500

    ok..ok

--- src/MainProvider.jsx
+++ src/MainProvider.jsx
@@ -6,7 +6,7 @@ import { MainContext } from "./MainContext";
 
 function MainProvider({ children }) {
   // const expressRoute = "https://qr-manager-server-gec1.onrender.com/";
-  const expressRoute = "https://server.qr-manager.net";
+  const expressRoute = "https://server.qr-manager.net/";
   // const expressRoute = "http://localhost:3000/";
   // const CLIENT_ID = process.env.CLIENT_ID;
   const [isLight, setIsLight] = useState(true);
